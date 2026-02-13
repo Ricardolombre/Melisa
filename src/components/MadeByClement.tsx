a65763d501dd861a658868df47f2929ba8897c43
Wildnis Shade Rich BATOUMENI <138714886+Ricardolombre@users.noreply.github.com>
2026-02-13 22:53:28 +0100

    Change text from 'Clement' to 'Ricardo'

--- src/components/MadeByClement.tsx
+++ src/components/MadeByClement.tsx
@@ -6,7 +6,7 @@ export const MadeByClement = () => {
   return (
     <div className="p-4 text-center">
       <span className="text-sm text-rose-400 font-serif italic">
-        Made by Clement ❤️
+        Made by Ricardo ❤️
       </span>
     </div>
   );
